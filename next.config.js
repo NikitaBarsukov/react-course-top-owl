b8606338463fe3255505857d7470c6fa75737884
Nikita Barsukov <nikita@Nikitas-MacBook-Pro-2.local>
2021-07-08 20:32:05 +0300

    fix svgr config for webpack

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-  webpack(config){
+  webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
-      use: ['@svgr/webpack'],
+      use: ["@svgr/webpack"]
+
     });
+
     return config;
-  },
+  }
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,22 @@
-import { AppProps } from "next/dist/next-server/lib/router/router";
-import "../styles/globals.css";
-import React from "react";
-import Head from "next/head";
+import { AppProps } from 'next/dist/next-server/lib/router/router';
+import '../styles/globals.css';
+import React from 'react';
+import Head from 'next/head';
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return (
-    <React.Fragment>
-      <Head>
-        <title>Залупа</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<Head>
+				<title>Top owl</title>
+				<link rel='preconnect' href='https://fonts.googleapis.com' />
+				<link rel='preconnect' href='https://fonts.gstatic.com' />
+				<link
+					href='https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap'
+					rel='stylesheet'
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</React.Fragment>
+	);
 }
 
 export default MyApp;
